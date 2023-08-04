48460b4089a6afa761d8723dfbc456dfd3eb9d6d
Kishore2800 <139871849+Kishore2800@users.noreply.github.com>
2023-08-04 10:17:59 +0530

    Update Admin_model.php
    Update Admin_model

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -8,6 +8,17 @@ class Admin_model extends CI_Model
         // Load the database library
         $this->load->database();
     }
+    public function get_admin_by_username_password($username, $password) {
+
+        $query = $this->db->get_where('admin', array('username' => $username, 'password' => $password));
+        if ($query->num_rows() === 1) {
+            return $query->row();
+
+        } else {
+            return FALSE;
+        }
+    }
+    
     public function getAllCandidates() {
         $query = $this->db->get('user'); // Assuming 'user' is the table name
         return $query->result();
@@ -16,10 +27,11 @@ class Admin_model extends CI_Model
     {
         // Retrieve a candidate by ID from the database
         $query = $this->db->get_where('user', array('id' => $id));
+
         return $query->row();
-        
+
     }
-   
+    
 }
 
 ?>
