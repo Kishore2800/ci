11092812c258f0ad9b0edb5cb492b77acd3be10a
Kishore2800 <139871849+Kishore2800@users.noreply.github.com>
2023-08-04 10:16:31 +0530

    Update admin_form.php
    Update admin_form

--- application/views/admin/admin_form.php
+++ application/views/admin/admin_form.php
@@ -1,251 +1,100 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html lang="en">
+<html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>admin | form</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <!-- Google Font: Source Sans Pro -->
+    <title>Admin Panel</title>
+    <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
+  <!-- Font Awesome -->
   <link rel="stylesheet" href="<?=base_url();?>assets/vendor/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?=base_url();?>assets/vendor/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?=base_url();?>assets/css/adminlte.min.css">
-  <!-- BS Stepper -->
-  <link rel="stylesheet" href="<?=base_url();?>assets/vendor/bs-stepper/css/bs-stepper.min.css">
-   <!-- daterange picker -->
-   <link rel="stylesheet" href="<?=base_url();?>assets/vendor/daterangepicker/daterangepicker.min.css">
+ <style>
+body, html {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.centered-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  border: 2px solid #ccc;
+  padding: 20px;
+  background-color: #f7f7f7;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+  </style>
 </head>
-<body class="hold-transition layout-top-nav">
-	<form action="<?php echo base_url('Admin/admin_form')?>" method="post">
-<div class="wrapper">
-
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md navbar-light navbar-maroon">
-    <div class="container">
-      <a href="../../index3.html" class="navbar-brand">
-        <img src="" alt="" class="" style="" >
-        <span class="brand-text font-weight-light"></span>
-      </a>
-
-      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-        <!-- Left navbar links -->
-              <!-- Level two dropdown-->
-                  <!-- Level three dropdown-->
-                  <!-- End Level three --> 
-              <!-- End Level two -->
-            </ul>
-          </li>
-        </ul>
-
-        <!-- SEARCH FORM -->
-      <!-- Right navbar links -->
-              <!-- Message End -->
-        <!-- Notifications Dropdown Menu -->     
-     
-  </nav>
-  <!-- /.navbar -->
-
+<body>
   <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+  <div class="centered-content">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container">
+    <section class="content-header">
+      <div class="container-fluid ">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0"> JAYA PRIYA GROUP OF COMPANIES <BR><small>DATA SHEET</small><BR></h1>
-			<h5 class="m-0">FOR OFFICE USE ONLY</h5>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?=base_url('Admin/admin_form');?>">Interviewer Form</a></li>
-              <li class="breadcrumb-item active">Form</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-   
-    <!-- Main content -->
-    <div class="content" >
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body" >
-              <div><?echo validation_errors()?></div>
-              <form action="<?php echo base_url('admin/admin_saveData')?>" method="POST">
-                <h5 class="card-title"><ins><b>RECRUITER</b></ins></h5><br><hr>
-                <div class="mb-3 row">
-                <div class="col-sm-4">
-<label class="lable">#apticant_id :- </label>
-</div>
-<div class="col-sm-3">
-<input type="text" name="apticant1" class="form-control" >
-</div></div>
-                <div class="mb-3 row">
-				<div class="col-sm-4">
-      <label class="lable">Interviewer Name:- </label>
-    </div>
-     <div class="col-sm-8">
-      <input type="text" name="inter_name" class="form-control" >
-    </div>
-</div>
-
-
-<div class="mb-3 row">
-				<div class="col-sm-4">
-      <label class="lable">Interviewer Designation:- </label>
-    </div>
-     <div class="col-sm-8">
-      <input type="text" name="inter_desig" class="form-control" >
-    </div>
-</div>
-
-
-
-<div class="mb-3 row">
-				<div class="col-sm-4">
-      <label class="lable">Interview Date:- </label>
-    </div>
-     <div class="col-sm-8">
-      <input type="Date" name="inter_date" class="form-control" >
-    </div>
-</div>
-
-
-
-<div class="mb-3 row">
-   <div class="col-sm-4">
-      <label class="lable">Interview Location:- </label>
-    </div>
-     <div class="col-sm-8">
-     <select name="inter_location" class="form-control" >
-      <option value="">Select Location</option>
-        <option value="Ariyalur">Ariyalur</option>
-        <option value="Chengalpattu">Chengalpattu</option>
-        <option value="Chennai">Chennai</option>
-		<option value="Coimbatore">Coimbatore</option>
-		<option value="Cuddalore">Cuddalore</option>
-		<option value="Dharmapuri">Dharmapuri</option>
-		<option value="Dindigul">Dindigul</option>
-		<option value="Erode">Erode</option>
-		<option value="Kallakurichi">Kallakurichi</option>
-		<option value="Kancheepuram">Kancheepuram</option>
-		<option value="Karur">Karur</option>
-		<option value="Krishnagiri">Krishnagiri</option>
-		<option value="Madurai">Madurai</option>
-		<option value="Mayiladuthurai">Mayiladuthurai</option>
-		<option value="Nagapattinam">Nagapattinam</option>
-		<option value="Nagercoil">Nagercoil</option>
-		<option value="Namakkal">Namakkal</option>
-		<option value="Perambalur">Perambalur</option>
-		<option value="Pudukkottai">Pudukkottai</option>
-		<option value="Ramanathapuram">Ramanathapuram</option>
-		<option value="Ranipet">Ranipet</option>
-		<option value="Salem">Salem</option>
-		<option value="Sivagangai">Sivagangai</option>
-		<option value="Tenkasi">Tenkasi</option>
-		<option value="Thanjavur">Thanjavur</option>
-		<option value="Theni">Theni</option>
-		<option value="Thiruvallur">Thiruvallur</option>
-		<option value="Thiruvarur">Thiruvarur</option>
-		<option value="Thoothukudi">Thoothukudi</option>
-    <option value="Tiruchirappalli">Tiruchirappalli</option>
-		<option value="Tirunelveli">Tirunelveli</option>
-		<option value="Tirupathur">Tirupathur</option>
-		<option value="Tiruvannamalai">Tiruvannamalai</option>
-		<option value="Udagamandalam">Udagamandalam</option>
-		<option value="Vellore">Vellore</option>
-		<option value="Viluppuram">Viluppuram</option>
-		<option value="Virudhunagar">Virudhunagar</option>
-     </select>
-    </div>
-    </div>
-
-
-	<div class="mb-3 row">
-   <div class="col-sm-4">
-      <label class="lable">Recommendation:- </label>
-    </div>
-     <div class="col-sm-8">
-     <select name="recommendation" class="form-control" >
-      <option value="">Select Recommendation</option>
-        <option value="Male">Selected on spot</option>
-        <option value="Female">Shortlisted</option>
-        <option value="Other">Hold</option>
-		<option value="Other">Rejected</option>
-     </select>
-    </div>
-    </div>
-
-
-	<div class="mb-3 row">
-				<div class="col-sm-4">
-      <label class="lable">Interviewer Note:- </label>
-    </div>
-     <div class="col-sm-8">
-	 <textarea type="text"  name="inter_note" class="form-control" ></textarea>
-    </div>
- </div>
-  </div>
-  <div class="container" style="text-align: center; display: flex; padding: 30px; margin: 20px; justify-content: center;">
-                <div class="btn btn-submit" >              
-                 <button type="submit" value="submit" class="btn btn-primary">Submit</button>
-                </div>
-              </div>
- </div>
- 
-</form>
-<!--second card-->
+          <div class="col-sm-12">
+            <h1>Login Dashboard</h1>
           </div>
-          <!-- /.col-md-6 -->
+          <div class="col-sm-6">
+          </div>
         </div>
-        <!-- /.row -->
       </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary ">
+    <div class="card-header text-center">
+    <h1><b>Admin</b>Login</a></h1>
     </div>
-    <!-- /.content -->
+    <div class="card-body ">
+      <p class="login-box-msg">Login to start your session</p>
+      <?php if(isset($error_message)) { ?>
+        <p><?php echo $error_message; ?></p>
+      <?php } ?>
+      <div><?echo validation_errors()?></div>
+      <form action="<?php echo base_url('Admin/login')?>" method="POST">
+        <div class="input-group mb-3">
+          <input type="text" name="username" class="form-control" placeholder="Username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">LogIn</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <!-- /.social-auth-links -->
+    </div>
+    <!-- /.card-body -->
   </div>
-  <!-- /.content-wrapper -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Powered by IT Team
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2023 <a href="https://jayapriyachits.com/">jayapriyachits.com</a>.</strong> All rights reserved.
-  </footer>
-</div>
 <!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- AdminLTE App -->
-<script src="<?=base_url();?>assets/vendor/js/adminlte.min.js"></script>
-<!-- jQuery -->
+ <!-- jQuery -->
 <script src="<?=base_url();?>assets/vendor/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="<?=base_url();?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?=base_url();?>assets/vendor/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?=base_url();?>assets/js/adminlte.min.js"></script>
-</form>
-</body>
+    <script src="<?= base_url('assets/js/admin.js') ?>"></script>
+    </body>
 </html>
